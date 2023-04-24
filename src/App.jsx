e2251d11974264b95812275ecb348317bb6e3961
Nasser Ben <nasserbenyed@gmail.com>
2023-04-23 22:55:17 -0700

    inital structure

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+import { Navbar, Menu, AboutUs, Gallery, Footer } from "./Components";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Menu />
+      <AboutUs />
+      <Gallery />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/index.js
@@ -0,0 +1,7 @@
+import Navbar from "./Navbar";
+import Menu from "./Menu";
+import AboutUs from "./AboutUs";
+import Gallery from "./Gallery";
+import Footer from "./Footer";
+
+export { Navbar, Menu, AboutUs, Gallery, Footer };
